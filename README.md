738a06aaf383dae410f252e18a53d9ecc695200c
unknown <asourav@live.com>
2021-03-11 16:27:57 +0600

    netlify error

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # World's top Football leagues
 
-##This project for my programming hero assignment-8 about react router
-###In this project I use api from thesportsdb and create a list of 15 world's top 15 leagues.
+## This project for my programming hero assignment-8 about react router
 
-[Website Live Link]()
+### In this project I use api from thesportsdb and create a list of 15 world's top 15 leagues.
+
+[Website Live Link](wonderful-bohr-6b49ee.netlify.app)
--- src/Components/LeagueDetails/LeagueDetails.js
+++ src/Components/LeagueDetails/LeagueDetails.js
@@ -12,7 +12,7 @@ import { faMapPin } from '@fortawesome/free-solid-svg-icons';
 import { faFlag } from '@fortawesome/free-solid-svg-icons';
 import { faFutbol } from '@fortawesome/free-solid-svg-icons';
 import { faMars } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
@@ -20,35 +20,35 @@ import maleImg from '../../img/male.png';
 import femaleImg from '../../img/female.png';
 
 const LeagueDetails = () => {
-    const {leagueId} = useParams();
+    const { leagueId } = useParams();
     const [league, setLeague] = useState({});
-    const [image, setImage] = useState(false);
+    // const [image, setImage] = useState(false);
     useEffect(() => {
         const url = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${leagueId}`;
         fetch(url)
-        .then(res => res.json())
-        .then(data => setLeague(data.leagues[0]))
-    } , [leagueId]);
+            .then(res => res.json())
+            .then(data => setLeague(data.leagues[0]))
+    }, [leagueId]);
 
     const showImage = () => {
         let gender = league.strGender;
         let image;
-        if(gender === 'Male'){
-            image = <img className="img-fluid" src={maleImg} alt=""/>
-        }else if(gender === 'Female'){
-            image = <img className="img-fluid" src={femaleImg} alt=""/>
+        if (gender === 'Male') {
+            image = <img className="img-fluid" src={maleImg} alt="" />
+        } else if (gender === 'Female') {
+            image = <img className="img-fluid" src={femaleImg} alt="" />
         }
         return image;
     }
-    
+
     return (
         <div>
             <section className="home-header">
-                <img width="150px" src={league.strBadge} alt=""/>
-            </section>            
+                <img width="150px" src={league.strBadge} alt="" />
+            </section>
             <Container>
                 <div className="league-summary">
-                    <Row>                           
+                    <Row>
                         <Col xs={12} md={6}>
                             <h3 className="league-name">{league.strLeague}</h3>
                             <ul>
@@ -57,13 +57,13 @@ const LeagueDetails = () => {
                                 <li><span><FontAwesomeIcon icon={faFutbol} /></span>Sport Type: {league.strSport}</li>
                                 <li><span><FontAwesomeIcon icon={faMars} /></span>Gender: {league.strGender}</li>
                             </ul>
-                        </Col>                       
+                        </Col>
                         <Col xs={12} md={6}>
-                            {                                  
+                            {
                                 showImage()
-                            }  
-                        </Col> 
-                    </Row>                                          
+                            }
+                        </Col>
+                    </Row>
                 </div>
                 <div className="description">
                     <p>{league.strDescriptionEN}</p>
@@ -76,7 +76,7 @@ const LeagueDetails = () => {
                     </ul>
                 </div>
                 <div className="back-home">
-                <Button className="main-btn"><Link to="/"> Back To Home <span><FontAwesomeIcon icon={faArrowRight} /></span></Link></Button>
+                    <Button className="main-btn"><Link to="/"> Back To Home <span><FontAwesomeIcon icon={faArrowRight} /></span></Link></Button>
                 </div>
             </Container>
         </div>
